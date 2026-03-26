3aa00018e94087b05d8d780bcf702752d646d108
Javed Mortuza <3372555+jmortuza@users.noreply.github.com>
2026-03-25 23:53:24 -0400

    ENG-5894: sdk 2.0.7 release (#14)

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SuperlogicWebViewKit",
-            url: "https://github.com/Superlogic/public-ios-sdk/releases/download/2.0.6/SuperlogicWebViewKit.xcframework.zip",
-            checksum: "d9ad79bda14a0bf9af753bfb41246c6b0037ace9afa895f25d5b2356bc0b0a10"
+            url: "https://github.com/Superlogic/public-ios-sdk/releases/download/2.0.7/SuperlogicWebViewKit.xcframework.zip",
+            checksum: "dd0a5d3b89f70c007e9c0721de296e871153b579ffc6df339a4ec89c00c1a6c0"
         ),
     ]
 )
