17463ebdaa985a9427f168cadd7bac238301a70e
Javed Mortuza <javed@superlogic.com>
2026-03-26 20:35:42 -0400

    ENG-5915: bump to release 2.0.8

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SuperlogicWebViewKit",
-            url: "https://github.com/Superlogic/public-ios-sdk/releases/download/2.0.7/SuperlogicWebViewKit.xcframework.zip",
-            checksum: "dd0a5d3b89f70c007e9c0721de296e871153b579ffc6df339a4ec89c00c1a6c0"
+            url: "https://github.com/Superlogic/public-ios-sdk/releases/download/2.0.8/SuperlogicWebViewKit.xcframework.zip",
+            checksum: "0d8537e86ede405739fae8f6d6e55e93519ca26e208ade82f2408eff3f804e69"
         ),
     ]
 )
