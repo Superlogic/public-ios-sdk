05de9be343858e4dfb7fad424d7dcaa5ef8d7894
Javed Mortuza <3372555+jmortuza@users.noreply.github.com>
2026-04-27 14:15:30 -0400

    ENG-6173: upgrade iOS SDK (#22)

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SuperlogicWebViewKit",
-            url: "https://github.com/Superlogic/public-ios-sdk/releases/download/2.0.12/SuperlogicWebViewKit.xcframework.zip",
-            checksum: "8adeb9c0b2790f1bf0c2ca5b6df045c22511e49832e11166afc0ae3f246acaca"
+            url: "https://github.com/Superlogic/public-ios-sdk/releases/download/2.0.13/SuperlogicWebViewKit.xcframework.zip",
+            checksum: "66a895c0218b1c146c8dd09a646bd8823422694d488d439343f4ea37b1a1eb04"
         ),
     ]
 )
